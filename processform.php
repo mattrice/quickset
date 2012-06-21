36352d332cbeede97d1daa26717fad85ab45f129
Bob Puffer <puffro01@Bobs-MacBook-Pro.local>
2012-06-21 11:09:46 -0500

    missing parentheses and couple other minor changes

--- processform.php
+++ processform.php
@@ -1,20 +1,23 @@
 <?php
+
+	ini_set('error_reporting', E_ALL);
+	ini_set('display_errors', 1);
     require_once('../../config.php');
 	global $CFG, $DB;
-//	global $COURSE;
+	global $COURSE;
 	global $USER;
     require_once($CFG->dirroot.'/lib/accesslib.php');
 	$courseid = required_param('courseid',PARAM_INT);
 	$returnurl = $_SERVER['HTTP_REFERER'];
 
-//	$shortname = $COURSE->shortname;
+	$shortname = $COURSE->shortname;
 
     $context = get_context_instance(CONTEXT_COURSE, $courseid);
     if ($data = data_submitted() and confirm_sesskey()) {
         $context = get_context_instance(CONTEXT_COURSE, $data->courseid);
         if (has_capability('moodle/course:update', $context)) {
         	$conditions = array('id' => $data->courseid);
-            if (!$course = $DB->get_record('course', $conditions) {
+            if (!$course = $DB->get_record('course', $conditions)) {
                     error('Course ID was incorrect');
             } else {
                 //// process making grades available data
@@ -42,3 +45,4 @@
 		}
     }
     redirect($returnurl);
+?>
