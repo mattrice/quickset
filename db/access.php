c42e6b48c6eb496e55220748f74f00760a804034
Matt Rice <mrice1@midmich.edu>
2017-05-01 17:00:09 -0400

    Add myaddinstance capability

--- db/access.php
+++ db/access.php
@@ -26,6 +26,16 @@ defined('MOODLE_INTERNAL') || die();
 
 $capabilities = array(
 
+    'block/quickset:myaddinstance' => array(
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes' => array(
+            'user' => CAP_ALLOW
+        ),
+
+        'clonepermissionsfrom' => 'moodle/my:manageblocks'
+    ),
+
     'block/quickset:addinstance' => array(
         'riskbitmask' => RISK_SPAM | RISK_XSS,
 
